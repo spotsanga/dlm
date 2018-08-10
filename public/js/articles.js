8987e455f029680ae8bfe0395c59598ca6fb00e5
root <root@ip-172-31-21-64.ap-south-1.compute.internal>
2018-08-10 10:20:45 +0530

    changes in articles.js

--- public/js/articles.js
+++ public/js/articles.js
@@ -11,13 +11,14 @@ function getArticles() {
             return;
         }
         move();
-        var code = '<thead><th>#</th><th>Source</th><th>Title</th><th>Description</th><th>Published At</th><th></th></thead>';
+        var code = '<thead><th>#</th><th>Source</th><th>Title</th><th>Description</th><th>Topic</th><th>Published At</th><th></th></thead>';
         for (var i = 0; i < articles.length; i++) {
             code += '<tr>';
             code += '<td><img class="rounded" src=' + articles[i]['urlToImage'] + ' height="100px" width="100px"></td>';
             code += '<td>' + articles[i]['name'] + '</td>';
             code += '<td>' + articles[i]['title'] + '</td>';
             code += '<td>' + (articles[i]['description'] || "") + '</td>';
+	    code += '<td>' + articles[i]['topic'] + '</td>';
             var date = new Date(articles[i]['publishedAt']);
             code += '<td>' + date.toDateString() + "</td>";
             code += '<td><a target="_blank" href="' + articles[i]['url'] + '"><i class="fas fa-external-link-alt"></i></td>';
