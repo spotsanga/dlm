82cfc8657e2967ca806bc60a16ff96d9dbdce096
johnpavis6 <johnpavis6@gmail.com>
2018-08-10 21:51:55 +0530

    changes in article.*

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,7 @@ class ArticleController extends Controller
             return response()->json(['data' => ['code' => '2', 'msg' => 'Session Expired']]);
         }
         $data['code'] = '0';
-        $data['articles']=Article::offset(0)->limit(10)->select('name','author','title','description','url','urlToImage','publishedAt')->get();
+        $data['articles']=Article::offset(0)->limit(30)->select('name','author','title','description','url','urlToImage','publishedAt')->get();
         return response()->json(['data' => $data]);
     }
 }
